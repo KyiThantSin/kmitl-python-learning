75cce38d3f3f5184d2d18f794c674093efa90036
KyiThantSin <kyits485@gmail.com>
2024-08-05 23:35:26 +0700

    exercies-folder

---
+++ exercises/ex00.py
@@ -0,0 +1,9 @@
+# display and calculation
+
+print("Welcome to python")
+print("Python is Fun")
+
+#convert Fahrenheit to Celsius
+fahrenheit = float(input("Enter Fahrenheit Value: "))
+celsius = 5 / 9 * (fahrenheit - 32)
+print("Fahrenheit", fahrenheit, "is", celsius, "Celsius")
