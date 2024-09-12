7481f56d7608ad217ae75e927e99030c71a1c032
KyiThantSin <kyits485@gmail.com>
2024-09-12 16:24:41 +0700

    week7

--- assignments/answers/week4/no2.py
+++ assignments/answers/week4/no2.py
@@ -2,9 +2,15 @@ class Poly:
     def __init__(self, x):
         self.x = x
 
-    def add(self,p):
-        print(self.x[0])
-    
+    def add(self, p):
+        result = []
+        max_len = max(len(self.x), len(p.x))
+        for i in range(max_len):
+            c1 = self.x[i] if i < len(self.x) else 0
+            c2 = p.x[i] if i < len(p.x) else 0
+            result.append(c1 + c2)
+        return Poly(tuple(result))
+
     def scalar_multiply(self, n):
         result = [value * n for value in self.x]
         return Poly(tuple(result))
@@ -18,6 +24,28 @@ class Poly:
                 result[i + j] += self.x[i] * p.x[j]
 
         return Poly(tuple(result))
+    
+    def power(self, n):
+        result = Poly((1,))
+        for _ in range(n):
+            result = result.multiply(self)
+        return result
+
+    def diff(self):
+        if len(self.x) <= 1:
+            return Poly((0,))
+        result = [i * self.x[i] for i in range(1, len(self.x))]
+        return Poly(tuple(result))
+
+    def integrate(self):
+        result = [0] + [self.x[i] / (i + 1) for i in range(len(self.x))]
+        return Poly(tuple(result))
+
+    def eval(self, n):
+        result = 0
+        for i in range(len(self.x)):
+            result += self.x[i] * (n ** i)
+        return result
 
     def print(self):
         result = ""
@@ -49,12 +77,24 @@ class Poly:
 
         print(result)
 
-
 p = Poly((1, 0, -2))
 p.print()
 
+q = p.power(2)
+q.print() 
+print(p.eval(3)) 
+     
+r = p.add(Poly((1, -2, 0)))
+r.print() 
+
+s = r.diff()
+s.print() 
+
 p2 = p.scalar_multiply(2)
 p2.print()
+
+p3 = p.add(p2)
+p3.print()
 
 r = p.multiply(p)
 r.print()
